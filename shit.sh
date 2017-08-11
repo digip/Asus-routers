a60b973a0cacabfe7c4d409c2bd5b8bc1a5682bf
digip <digip@offsec.com>
2017-08-10 21:41:27 -0400

    Create shit.sh

---
+++ shit.sh
@@ -0,0 +1,28 @@
+#	Disable and kill listening services on Asus routers
+#	enable telnet, then login
+telnet 192.168.1.1 23
+
+#turn spanning tree protoocol off(if you don't need it, which most of you won't at home)
+nvram set lan_stp=0
+nvram set lan0_stp=0
+#disable USB printer (Most printers are wireless these days, no need to be tethered to router, nor sharing the pritners drive with the web over FTP!!)
+nvram set usb_printer=0
+#disable wan down redirect page (This can also be done in the admin pane, but just set here while in)
+nvram set web_redirect=0
+#commit channges
+nvram commit
+reboot
+
+#Then kill all these running programs
+killall -9 nas #network attached storage, kill if not using
+killall -9 u2ec #usb to ethernet connector, who uses this???
+killall -9 wanduck	#fuck a duck, unless you have more than one WAN, kill this
+killall -9 watchdog	#no dogs allowed, ok to stop this
+killall -9 wpsaide	#wireless protected shite, STFU, GTFO
+killall -9 mDNSNetMonitor #no apple shite on my network!
+service stop_lpd #line printer services on a router. why? I'm not printing or logging shite
+#now run netstat
+netstat -antlu	#DNS and your HTTPS port for administration are all you need. port 23 can close after you disable telnet again.
+#you should now have quieted down your router and closed unneccesary ports
+#rebooting will start the programs above, but not the nvram set items, kill them each reboot to close those ports
+#if you find any of these issues cause problems for you, then reverse the nvram set to 1, reboot, all should be back to default.
